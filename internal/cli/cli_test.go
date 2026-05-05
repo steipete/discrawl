d1f4d378f761438a586299b935e98eae61af8009
Peter Steinberger <steipete@gmail.com>
2026-05-05 01:47:34 +0100

    release: v0.6.6

--- internal/cli/cli_test.go
+++ internal/cli/cli_test.go
@@ -34,7 +34,7 @@ func TestHelpAndVersion(t *testing.T) {
 
 	out.Reset()
 	require.NoError(t, Run(context.Background(), []string{"--version"}, &out, &bytes.Buffer{}))
-	require.Contains(t, out.String(), "0.6.5")
+	require.Contains(t, out.String(), "0.6.6")
 
 	err := Run(context.Background(), []string{"bogus"}, &out, &bytes.Buffer{})
 	require.Equal(t, 2, ExitCode(err))
--- internal/cli/version.go
+++ internal/cli/version.go
@@ -1,3 +1,3 @@
 package cli
 
-var version = "0.6.5"
+var version = "0.6.6"
